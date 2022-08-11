dfeb49994f3ebf8f46d34101a1c68d146013ddfc
Hemanth <hemanthr2053@gmail.com>
2022-08-11 17:14:04 +0530

    task updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Popup from './Pages/Popup';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <nav class="navbar bg-primary">
+      <div class="container-fluid">
+        
+        <h4>View Audience</h4>
+      </div>
+    </nav>
+      <div class="butto">  
+      <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Segment
+      </button>
+      </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+           <Popup/>
+            
+         </div>
+        </div>
+      </div>
+    </div>
+
     </div>
   );
 }
---
+++ src/Pages/Popup.js
@@ -0,0 +1,124 @@
+
+import "./Popup.css";
+import { useState } from "react";
+import axios from 'axios';
+
+const Dummy_data = [
+    { Lable: "First Name", Value: "first_name", showing: true, selected: true },
+    { Lable: "Last Name", Value: "last_name", showing: false, selected: false },
+    { Lable: "Gender", Value: "gender", showing: false, selected: false },
+    { Lable: "Age", Value: "age", showing: false, selected: false },
+    { Lable: "Account Name", Value: "account_name", showing: true, selected: true },
+    { Lable: "City", Value: "city", showing: false, selected: false },
+    { Lable: "State", Value: "state", showing: false, selected: false },
+];
+
+function App() {
+    const [data, setData] = useState(Dummy_data);
+    const [addNewSchema, setAddNewSchema] = useState("");
+    const [segmentName, setSegmantName] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    const initialState = {
+        first_name: "",
+        last_name: "",
+        gender: "",
+        age: "",
+        account_name: "",
+        city: "",
+        state: "",
+
+    };
+
+    const [segmanetData, setSegmantData] = useState(initialState);
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setSegmantData({ ...segmanetData, [e.target.name]: e.target.value });
+    };
+
+    const handleSchemaSubmit = (e) => {
+        e.preventDefault();
+        let changeData = [...data];
+
+        changeData.forEach((d) => {
+            if (d.Lable === addNewSchema) {
+                d.showing = true;
+            }
+        });
+        setData(changeData);
+    };
+
+    const handleRemoveSegment = (label) => {
+        let removeData = [...data];
+
+        removeData.forEach((d) => {
+            if (d.Lable === label) {
+                d.showing = false;
+            }
+        });
+
+        setData(removeData);
+    };
+
+    const handleSegmentSave = async function (e) {
+        e.preventDefault();
+        const TotalSegment = {
+            segment_name: segmentName,
+            schema: { ...segmanetData },
+        };
+        console.log(TotalSegment)
+        await axios.post("https://webhook.site/aae553f1-45f4-4bda-941c-2f3ebaa167e1", TotalSegment
+        ).then((res) => console.log(res)).catch((er) => console.log(er))
+    };
+
+
+
+    return (
+        <div className="comp">
+            <h5>Saving Segment</h5>
+            <label>Enter the name of the segment</label>
+            <input placeholder="Segment Name"
+                onChange={(e) => setSegmantName(e.target.value)}
+            ></input>
+            <p>To save your segment you need to add your schemas to build the query</p>
+            <form>
+                {data
+                    .filter((d) => d.showing === true)
+                    .map((d) => (
+                        <div className="cont" key={d.Lable}>
+                            <select className="sel" onChange={handleChange} name={d.Value}>
+                                <option>{d.Lable}</option>
+                                {data
+                                    .filter((f) => !Object.keys(segmanetData).includes(f.Lable))
+                                    .map((k) => (
+                                        <option key={k.Lable}>{k.Lable}</option>
+                                    ))}
+                            </select>
+                            <button className="btn-one" onClick={() => handleRemoveSegment(d.Lable)}>-</button>
+                        </div>
+                    ))}
+            </form>
+
+            <form onSubmit={handleSchemaSubmit}>
+                <select className="form-cont" onChange={(e) => setAddNewSchema(e.target.value)}>
+                    {data.map((d) => (
+                        <option className="sel" key={d.Lable}>{d.Lable}</option>
+                    ))}
+                </select>
+
+                <a>
+                    <button className="btn-one" type="submit">+Add new schema</button>
+                </a>
+
+            </form>
+            <div className="btn-cont">
+                <button className="btn-two" onClick={(e) => handleSegmentSave(e)}>Save the segment</button>
+                <button className="btn-three">Cancel</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
